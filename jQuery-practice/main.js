6dce8e8aa49267f5d4329de09839ecae40dba8b9
Nicole Boettcher <11392575+deliciousnachos@users.noreply.github.com>
2018-02-21 21:36:31 -0500

    adding crap

--- jQuery-practice/main.js
+++ jQuery-practice/main.js
@@ -1,1 +1,20 @@
 console.log('you did it')
+
+
+
+//do in console 
+
+
+
+
+//create new div 
+const $newDiv = $('<div>');
+
+// Add hipster ipsum content
+$newDiv.html("Farm-to-table Godard roof party bespoke, fashion axe mustache vinyl.");
+
+// Set it's class to innerItem
+$newDiv.addClass("inner-item");
+
+// Append our new element  
+$('#outer-container').append($newDiv);
